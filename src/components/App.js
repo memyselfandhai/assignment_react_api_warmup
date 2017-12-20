8d00936bc97840e08ea97a96e28e04c114a23de2
Elias Hantula <ehantula@indiana.edu>
2017-12-20 12:11:06 -0500

    usercreation

--- src/components/App.js
+++ src/components/App.js
@@ -13,17 +13,11 @@ class App extends Component {
       users: [],
       isFetching: false
     };
+
+
   }
 
-  // New add user action
-  onAddUser = e => {
-    e.preventDefault();
-    const form = e.target;
-    const body = serialize(form, { hash: true });
-    console.log(body);
-  };
-
-  componentDidMount() {
+componentDidMount() {
     this.setState({ isFetching: true });
     fetch("https://reqres.in/api/users?delay=1")
       .then(response => response.json())
@@ -33,10 +27,64 @@ class App extends Component {
           isFetching: false
         });
       });
+}
+  // New add user action
+  onAddUser = e => {
+    e.preventDefault();
+    const form = e.target;
+    const body = serialize(form, { hash: true });
+    console.log(body);
+  ;
+
+const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+
+
+const options = {
+      headers,
+      method: 'POST',
+      body: JSON.stringify(body),
+    }
+
+   this.setState({isFetching: true})
+
+    fetch('https://reqres.in/api/users', options)
+      .then((response) => {
+        // If response not okay, throw an error
+        if (!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`)
+        }
+
+        // Otherwise, extract the response into json
+        return response.json()
+      })
+      .then((json) => {
+        // Update the user list and isFetching.
+        // Reset the form in a callback after state is set.
+        this.setState({
+          isFetching: false,
+          users: [
+            ...this.state.users,
+            json,
+          ]
+        }, () => { form.reset() })
+      })
+      .catch((error) => {
+        // Set error in state & log to console
+        console.log(error)
+        this.setState({
+          isFetching: false,
+          error,
+        })
+      })
+  
+
+
+
   }
 
   render() {
-    const { users, isFetching } = this.state;
+    const { users, isFetching, error } = this.state;
 
     return (
       <div className="App">
@@ -46,7 +94,9 @@ class App extends Component {
         />
         <UserList users={users} isFetching={isFetching} />
         <br />
-        <UserForm onSubmit={this.onAddUser} />
+        <UserForm onSubmit={this.onAddUser} 
+         error={error}
+        />
       </div>
     );
   }
